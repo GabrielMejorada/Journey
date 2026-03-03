075742b36c2f79d47fadc5a2bf6c124b67dcac1d
gabrielmejorada <gabrielmejorada12@gmail.com>
2026-03-03 12:16:20 -0700

    Worked on home section, added animation, WIP JS

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded',()=>{
+  function placeholderFunction (){
+    const placeholderConst = document.getElementById("greeterAnimation");
+
+    placeholderConst.textContent = "Test";
+    console.log("This works")
+  }
+});
+
