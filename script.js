04398747bb465388e639a9a9da85af36081fdcf2
gabrielmejorada <gabrielmejorada12@gmail.com>
2026-03-24 15:13:07 -0600

    Refined greeter, refined about section, finished vaporwave section

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
-  const element = document.getElementById("greeterAnimation");
-  const fullText = "Welcome to my website!! ^.^";
+document.addEventListener('DOMContentLoaded', () => {
+  const line1 = document.getElementById("greeterAnimation");
+  const line2 = document.getElementById("scrollAnimation");
+  
+  const text1 = "Welcome to my website!! ^.^";
+  const text2 = "Scroll down if you want to see more!";
+  
   let i = 0;
-  let typingTimer;
+  let j = 0;
 
-  function type() {
-    if (i < fullText.length) {
-      element.textContent += fullText.charAt(i);
+  function typeLine1() {
+    if (i < text1.length) {
+      line1.textContent += text1.charAt(i);
       i++;
-      typingTimer = setTimeout(type, 100);
+      setTimeout(typeLine1, 100);
+    } else {
+      setTimeout(typeLine2, 500); 
     }
   }
-  
-  element.textContent = "";
-  type();
 
-  document.querySelector
+  function typeLine2() {
+    if (j < text2.length) {
+      line2.textContent += text2.charAt(j);
+      j++;
+      setTimeout(typeLine2, 50);
+    }
+  }
 
+  line1.textContent = "";
+  line2.textContent = "";
+  typeLine1();
 });
-
